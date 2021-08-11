c89c9282b69380838c015549251e35b4d3a24865
IvyZheng85 <ivy.zheng85@gmail.com>
2021-08-11 16:13:38 -0700

    deploy

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local Apps
-    'images',
+    'images',  
     'api',
     # 3rd party
     # 'rest_framework',
